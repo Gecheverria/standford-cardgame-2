2e239f9a800a185053a03ea7adb84de53c88e38a
Gabriel Enrique Echeverria Mira <Gabriel@gabriels-macbook-pro.local>
2016-09-09 18:02:16 -0600

    Initial Commit

---
+++ CardGame/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CardGame
+//
+//  Created by Gabriel Enrique Echeverria Mira on 9/9/16.
+//  Copyright © 2016 Gabriel Enrique Echeverria Mira. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CardGame/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CardGame
+//
+//  Created by Gabriel Enrique Echeverria Mira on 9/9/16.
+//  Copyright © 2016 Gabriel Enrique Echeverria Mira. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
